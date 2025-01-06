36695e5cb2673681cdc3973ba0c6edf77a82a829
anonymous203030 <aram.simonyan.03@gmail.com>
2025-01-07 01:03:58 +0400

    Create Session for endpoints use

--- main/database.py
+++ main/database.py
@@ -1,6 +1,8 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Annotated
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 # Create books.db sqlite DB file
 engine = create_async_engine("sqlite+aiosqlite:///books.db")
@@ -12,3 +14,6 @@ new_session = async_sessionmaker(bind=engine)
 async def get_session() -> AsyncGenerator:
     async with new_session() as session:
         yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
