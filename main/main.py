5660c272a239aa10f783dfab9a71222708bec895
anonymous203030 <aram.simonyan.03@gmail.com>
2025-01-06 22:42:16 +0400

    GET books request added (test version)

--- main/main.py
+++ main/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
 
 app = FastAPI()
 
+books = [
+    {
+        "id": 1,
+        "title": "Асинхронность в Python",
+        "author": "Mathew"
+    },
+    {
+        "id": 2,
+        "title": "Backend разработка в Python",
+        "author": "Artyom"
+    }
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
+@app.get("/books")
+def read_books():
+    return books
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
