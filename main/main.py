5d204a4540e1fd8b027f1d2702d955669afd08d2
anonymous203030 <aram.simonyan.03@gmail.com>
2025-01-06 22:43:01 +0400

    GET book by id request added (test version)

--- main/main.py
+++ main/main.py
@@ -23,3 +23,12 @@ books = [
 def read_books():
     return books
 
+
+@app.get("/books/{book_id}")
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
