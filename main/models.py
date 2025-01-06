73c1e063cc71ee5cdf2a37d7ec39f0f711cc8c2c
anonymous203030 <aram.simonyan.03@gmail.com>
2025-01-07 02:32:10 +0400

    User model added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from sqlalchemy.types import String, Integer
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 from main.database import engine
@@ -20,3 +23,16 @@ async def setup_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(120), nullable=False)
+    name: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    surname: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    bio: Mapped[Optional[str]] = mapped_column(String(100), nullable=True)
+    age: Mapped[Optional[int]] = mapped_column(Integer, nullable=True)
+    gender: Mapped[Optional[str]] = mapped_column(String(10), nullable=True)
